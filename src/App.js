65b74aaf0115816557b8b4189e378c191a071628
unknown <saminter22@gmail.com>
2021-12-14 14:18:56 +0400

    С изменениями . Food

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+const foodILike = [
+  {
+    'id': 1,
+    'name': 'борщ',
+    'image': 'https://2recepta.com/recept/borshh/borshh.jpg',
+    'rating': 5.0
+  },
+  {
+    'id': 2,
+    'name': 'драники',
+    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/Draniki_1527664015.jpg',
+    'rating': 4.7
+  },
+  {
+    'id': 3,
+    'name': 'стейк',
+    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/20-laJfxakov-dlya-prigotovleniya-deJstvitelno-bozhestvennogo-steJka_1526682003-1140x570.jpg',
+    'rating': 4.9
+  },
+  {
+    'id': 4,
+    'name': 'хинкали', 
+    'image': 'https://vosmarket.ru/uploads/posts/2018-11/1541331854_vosmarket_ru-xinkali-0.jpg',
+    'rating': 3.5
+  }
+]
+
+function Food({name, picture, rating}){
+  return (
+    <div>
+      <h3>Я люблю {name}</h3>
+      <h4>{rating} / 5.0</h4>
+      <img src= {picture} alt="" />
+    </div> 
+  );
+}
+
+Food.propTypes = {
+name: PropTypes.string.isRequired,
+picture: PropTypes.string.isRequired,
+rating: PropTypes.number.isRequired
+
+}
+
+function renderFood (dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {console.log(foodILike.map(renderFood))}
+      {foodILike.map(renderFood)}
+
     </div>
+
   );
 }
 
