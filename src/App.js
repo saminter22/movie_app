8b8742fc56cdf1e92fbd20b78486db3225ebb6d7
unknown <saminter22@gmail.com>
2021-12-15 18:00:31 +0400

    С изменениями . Movies

--- src/App.js
+++ src/App.js
@@ -1,62 +1,57 @@
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-const foodILike = [
-  {
-    'id': 1,
-    'name': 'борщ',
-    'image': 'https://2recepta.com/recept/borshh/borshh.jpg',
-    'rating': 5.0
-  },
-  {
-    'id': 2,
-    'name': 'драники',
-    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/Draniki_1527664015.jpg',
-    'rating': 4.7
-  },
-  {
-    'id': 3,
-    'name': 'стейк',
-    'image': 'https://cdn.lifehacker.ru/wp-content/uploads/2018/05/20-laJfxakov-dlya-prigotovleniya-deJstvitelno-bozhestvennogo-steJka_1526682003-1140x570.jpg',
-    'rating': 4.9
-  },
-  {
-    'id': 4,
-    'name': 'хинкали', 
-    'image': 'https://vosmarket.ru/uploads/posts/2018-11/1541331854_vosmarket_ru-xinkali-0.jpg',
-    'rating': 3.5
+class App extends React.Component {
+  
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      'https://yts.mx/api/v2/list_movies.json?sort_by=rating'
+      );
+    this.setState({ movies, isLoading: false });
   }
-]
 
-function Food({name, picture, rating}){
-  return (
-    <div>
-      <h3>Я люблю {name}</h3>
-      <h4>{rating} / 5.0</h4>
-      <img src= {picture} alt="" />
-    </div> 
-  );
-}
+  componentDidMount(){
+    this.getMovies();
+    }
 
-Food.propTypes = {
-name: PropTypes.string.isRequired,
-picture: PropTypes.string.isRequired,
-rating: PropTypes.number.isRequired
-
-}
-
-function renderFood (dish){
-  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-}
-
-function App() {
-  return (
-    <div>
-      {console.log(foodILike.map(renderFood))}
-      {foodILike.map(renderFood)}
-
-    </div>
-
-  );
+  render(){
+    const {isLoading, movies} = this.state;
+    return (
+    <section className = "container">
+      {isLoading ? (
+        <div className="loader">
+            <span className="loader_text">Загрузка...</span>
+        </div>
+       ) : (
+        <div className="movies">
+          {movies.map((movie) => (
+              <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+        </div>
+      )}
+    </section>
+    );
+  }
 }
 
 export default App;
